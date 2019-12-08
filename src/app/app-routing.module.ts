791e1460c60efb15c4a9c20f2791e2d4ea5fc7e2
Zeppl Studios <mayke.eng@outlook.com>
2019-12-08 13:24:14 +0000

    Add Home and NotFoundPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    // canActivate: [AuthGuard],
+    // children: [
+    //   {
+    //     path: 'seller',
+    //     component: SellerComponent
+    //   },
+    //   {
+    //     path: 'seller/:id',
+    //     component: SellerComponent
+    //   },
+    //   {
+    //     path: 'seller/product/:id',
+    //     component: ProductComponent
+    //   }
+    // ]
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
