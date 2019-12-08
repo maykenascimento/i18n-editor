a23bbf851635eb59e534f093a5f87a9245e3b599
Zeppl Studios <mayke.eng@outlook.com>
2019-12-08 16:28:54 +0000

    First elements on the homepage

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -10,7 +10,49 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.projects = [{}];
+    this.projects = [{
+      "projectId": 1,
+      "title": "Span",
+      "languages": [
+        {
+          "name": "lb"
+        }
+      ],
+      "terms": 460
+    }, {
+      "projectId": 2,
+      "title": "Kanlam",
+      "languages": [
+        {
+          "name": "ro"
+        },
+        {
+          "name": "nl"
+        }
+      ],
+      "terms": 12
+    }, {
+      "projectId": 3,
+      "title": "Konklux",
+      "languages": [
+        {
+          "name": "mh"
+        },
+        {
+          "name": "de"
+        },
+        {
+          "name": "be"
+        },
+        {
+          "name": "pt"
+        }
+      ],
+      "terms": 567
+    }];
   }
 
+  openProject(project) {
+    alert('asdfklj');
+  }
 }
